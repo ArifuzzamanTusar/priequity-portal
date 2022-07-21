f2deb91cad218571619ad0657561120e8ee49dc3
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-07-22 00:22:24 +0600

    cleanded

--- Classes/function.php
+++ Classes/function.php
@@ -27,10 +27,6 @@ class DbClass
     private $documentsTable = 'documents';
     private $portalTable = 'portal_option';
 
-
-    private $invoiceOrderTable = 'invoice_order';
-    private $invoiceOrderItemTable = 'invoice_order_item';
-    private $customerTable = 'customer';
     private $dbConnect = false;
 
 
@@ -93,118 +89,6 @@ class DbClass
         }
     }
 
-
-
-
-    // --------------  SAVE INVOICE ---------------
-
-
-    public function saveInvoice($POST, $currentNumber)
-    {
-        $sqlInsert = "
-			INSERT INTO " . $this->invoiceOrderTable . "(user_id, order_receiver_name, order_receiver_address, order_total_before_tax, order_total_tax,order_due_date, order_date, inv_ref, order_total_after_tax) 
-			VALUES ('" . $POST['userId'] . "', '" . $POST['companyName'] . "', '" . $POST['address'] . "', '" . $POST['netAmount'] . "', '" . $POST['taxAmount'] . "','" . $POST['due_date'] . "','" . $POST['order_date'] . "','" . $POST['ref'] . "', '" . $POST['subTotal'] . "')";
-        mysqli_query($this->dbConnect, $sqlInsert);
-
-
-        $lastInsertId = mysqli_insert_id($this->dbConnect);
-        for ($i = 0; $i < count($POST['productName']); $i++) {
-            $sqlInsertItem = "
-			INSERT INTO " . $this->invoiceOrderItemTable . "(order_id, item_disc, item_name, order_item_quantity,uom, order_item_price,tax_percent,tax_amount, order_item_final_amount,date) 
-			VALUES ('" . $currentNumber . "', '" . $POST['productDisc'][$i] . "', '" . $POST['productName'][$i] . "', '" . $POST['quantity'][$i] . "', '" . $POST['uom'][$i] . "', '" . $POST['price'][$i] . "','" . $POST['tax'][$i] . "', '" . $POST['taxtot'][$i] . "',  '" . $POST['total'][$i] . "',  '" . $POST['date'][$i] . "')";
-            mysqli_query($this->dbConnect, $sqlInsertItem);
-        }
-
-        // echo $currentNumber;
-    }
-
-
-    /*
-	public function updateInvoice($POST) {
-		if($POST['invoiceId']) {	
-			$sqlInsert = "
-				UPDATE ".$this->invoiceOrderTable." 
-				SET order_receiver_name = '".$POST['companyName']."', order_receiver_address= '".$POST['address']."', order_total_before_tax = '".$POST['subTotal']."', order_total_tax = '".$POST['taxAmount']."', order_tax_per = '".$POST['taxRate']."', order_total_after_tax = '".$POST['totalAftertax']."', order_amount_paid = '".$POST['amountPaid']."', order_total_amount_due = '".$POST['amountDue']."', note = '".$POST['notes']."' 
-				WHERE user_id = '".$POST['userId']."' AND order_id = '".$POST['invoiceId']."'";		
-			mysqli_query($this->dbConnect, $sqlInsert);	
-		}		
-		$this->deleteInvoiceItems($POST['invoiceId']);
-		for ($i = 0; $i < count($POST['productCode']); $i++) {			
-			$sqlInsertItem = "
-				INSERT INTO ".$this->invoiceOrderItemTable."(order_id, item_disc, item_name, order_item_quantity, order_item_price, order_item_final_amount) 
-				VALUES ('".$POST['invoiceId']."', '".$POST['productCode'][$i]."', '".$POST['productName'][$i]."', '".$POST['quantity'][$i]."', '".$POST['price'][$i]."', '".$POST['total'][$i]."')";			
-			mysqli_query($this->dbConnect, $sqlInsertItem);			
-		}       	
-	}	
-
-	*/
-    public function getInvoiceList()
-    {
-        $sqlQuery = "
-			SELECT * FROM " . $this->invoiceOrderTable . " 
-			WHERE user_id = '" . $_SESSION['userid'] . "'";
-        return  $this->getData($sqlQuery);
-    }
-    public function getInvoice($invoiceId)
-    {
-        $sqlQuery = "
-			SELECT * FROM " . $this->invoiceOrderTable . " 
-			WHERE user_id = '" . $_SESSION['userid'] . "' AND order_id = '$invoiceId'";
-        $result = mysqli_query($this->dbConnect, $sqlQuery);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        return $row;
-    }
-    public function getInvoiceItems($invoiceId)
-    {
-        $sqlQuery = "
-			SELECT * FROM " . $this->invoiceOrderItemTable . " 
-			WHERE order_id = '$invoiceId'";
-        return  $this->getData($sqlQuery);
-    }
-
-
-    // ---------------- DELETING PROCESS -------------
-    public function deleteInvoiceItems($invoiceId)
-    {
-        $sqlQuery = "
-			DELETE FROM " . $this->invoiceOrderItemTable . " 
-			WHERE order_id = '" . $invoiceId . "'";
-        mysqli_query($this->dbConnect, $sqlQuery);
-    }
-    public function deleteInvoice($invoiceId)
-    {
-        $sqlQuery = "
-			DELETE FROM " . $this->invoiceOrderTable . " 
-			WHERE order_id = '" . $invoiceId . "'";
-        mysqli_query($this->dbConnect, $sqlQuery);
-        $this->deleteInvoiceItems($invoiceId);
-        return 1;
-    }
-
-
-
-    // ---------------------------------------AFTER THE HELL -------------------------------------
-
-    public function allCustomerList()
-    {
-        $sqlQuery = "SELECT * FROM " . $this->customerTable . "";
-        return  $this->getData($sqlQuery);
-    }
-    public function currentInvoiceNumber()
-    {
-        $sqlQuery = "SELECT * FROM " . $this->invoiceOrderTable . " ORDER BY order_id DESC LIMIT 1";
-        return  $this->getData($sqlQuery);
-    }
-
-
-
-    // =======|||| EXTRAS ||||=======
-    // -------Country Data
-    public function countryData()
-    {
-        $sqlQuery = "SELECT * FROM " . $this->countryTable . " ";
-        return  $this->getData($sqlQuery);
-    }
 
 
 
@@ -473,7 +357,7 @@ class DbClass
         //Create an instance; passing `true` enables exceptions
         $mail = new PHPMailer(true);
         $portalOption = $this->getData("SELECT * FROM " . $this->portalTable . " WHERE `site_key` LIKE '2d58745e5af8524a5c0f9366ab25d493f98b160f'")[0];
-       
+
 
 
         try {
@@ -573,5 +457,15 @@ class DbClass
         }
 
         return $text;
+    }
+
+
+
+    // =======|||| EXTRAS ||||=======
+    // -------Country Data
+    public function countryData()
+    {
+        $sqlQuery = "SELECT * FROM " . $this->countryTable . " ";
+        return  $this->getData($sqlQuery);
     }
 }
